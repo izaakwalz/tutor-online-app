12e8926728ce62219a863b5ebf5e9fb10f2dd31b
izaakwalz <iblack.xyz@gmail.com>
2021-06-19 18:46:46 +0100

    added store provider, also broswer router

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
+import { CssBaseline } from '@material-ui/core';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import store from './store';
+import 'material-react-toastify/dist/ReactToastify.css';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <BrowserRouter>
+        <CssBaseline />
+        <App />
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
---
+++ src/store.js
@@ -0,0 +1,35 @@
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import {
+  subjectDetailsReducer,
+  subjectListReducer,
+} from './reducer/subject.reducers';
+import { userLoginReducer, userRegisterReducer } from './reducer/auth.reducers';
+import {
+  listProgressReducer,
+  progressDetailsReducer,
+  recordGradeReducer,
+} from './reducer/progress.reducers';
+
+const reducer = combineReducers({
+  subjectList: subjectListReducer,
+  subjectDetails: subjectDetailsReducer,
+  userLogin: userLoginReducer,
+  userRegister: userRegisterReducer,
+  scoreDetails: progressDetailsReducer,
+  recordScore: recordGradeReducer,
+  userGrade: listProgressReducer,
+});
+
+const initialState = {};
+
+const middleware = [thunk];
+
+const store = createStore(
+  reducer,
+  initialState,
+  composeWithDevTools(applyMiddleware(...middleware))
+);
+
+export default store;
