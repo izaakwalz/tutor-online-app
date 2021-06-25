c1d608b689c729a070265623919cfc0207b28899
izaakwalz <iblack.xyz@gmail.com>
2021-06-25 18:20:34 +0100

    reuseable components for Home page

---
+++ src/components/elements/index.js
@@ -0,0 +1,8 @@
+export { default as Image } from './Image';
+export { default as Icon } from './Icon';
+export { default as LearnMoreLink } from './LearnMoreLink';
+export { default as SectionHeader } from "./SectionHeader";
+export { default as Section } from './Section';
+export { default as SectionAlternate } from './SectionAlternate';
+export { default as CardBase } from './CardBase';
+export { default as CardPricingStandard } from './CardPricingStandard';
