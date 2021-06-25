5f6d27f1f6f8f50c5f13937ab9c6164c465d3b74
izaakwalz <iblack.xyz@gmail.com>
2021-06-25 18:19:46 +0100

    Added routes to app.js and redux state mangement

---
+++ src/pages/index.js
@@ -0,0 +1,6 @@
+// export { default as HomePage } from './Home';
+export { default as HomePage } from './Home';
+export { default as SubjectPage } from './Subject';
+export { default as AuthPage } from './Auth';
+export { default as SubjectListPage } from './SubjectList';
+export { default as NotFoundPage } from './NotFound';
